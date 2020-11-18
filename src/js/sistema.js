f18cd36380dc3b93cfbc535201b0113ec13a1aa9
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-11-17 23:46:27 -0300

    carga de lecciones

--- src/js/sistema.js
+++ src/js/sistema.js
@@ -1,42 +1,33 @@
 import Alumno from './alumno.js';
 import Profesor from './profesor.js';
 import Leccion from './leccion.js';
-import Historial from './historial.js';
+//import Historial from './historial.js';
 
 export default class Sistema {
-    
-    constructor() {
-      
-      this.listaProfesores = new ArrayList();
-      this.listaAlumnos = new ArrayList();
-      this.listaLecciones = new ArrayList();
-      this.listaHistorial = new ArrayList();
 
-    }
+  constructor() {
 
+    this.listaProfesores = [];
+    this.listaAlumnos = [];
+    this.listaLecciones = [];
+    this.listaHistorial = [];
+    this.datosPrueba();
+  }
 
-     datosPrueba(){ 
+  datosPrueba() {
 
-      var alumno1 = new Alumno("Ramael");
-      var profesor1 = new Profesor("Alberto");
-      this.listaProfesores.add(profesor1);
-      this.listaAlumnos.add(alumno1);
-        
-    }
-
-
-     cargarLeccion(nombre, descripcion, imagen, tablatura, autor){
-
-        alert("leccion cargada");
-        var leccion = new Leccion(nombre,descripcion,imagen,tablatura,autor);
-        this.listaLecciones.add(leccion);
-
-        
-     }
-
-
-    
-    
-
+    var alumno1 = new Alumno("Ramael");
+    var profesor1 = new Profesor("Alberto");
+    this.listaProfesores.push(profesor1);
+    this.listaAlumnos.push(alumno1);
 
   }
+
+  cargarLeccion(nombre, descripcion, imagen, tablatura, autor) {
+
+    alert("leccion cargada");
+    var leccion = new Leccion(nombre, descripcion, imagen, tablatura, autor);
+    this.listaLecciones.add(leccion);
+  }
+
+}
