068fab9593ab9bfb79dab021f91c4d3bc87f84b4
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-11-17 21:39:24 -0300

    formulario creacion lecciones

--- src/pages/crearLecciones/crearLecciones.js
+++ src/pages/crearLecciones/crearLecciones.js
@@ -1,5 +1,10 @@
 document.getElementById("btn-crear-leccion").addEventListener("click", mostrarModal);
 document.getElementById("btn-ocultar-modal").addEventListener("click", ocultarModal);
+document.getElementById("imp-file").addEventListener("click", seleccionarImagen);
+document.getElementById("fil-img-leccion").addEventListener("change", tomarImagen);
+
+const impSeleccionImg = document.getElementById("fil-img-leccion");
+const txtSeleccionImg = document.getElementById("imagen-seleccionada");
 
 function mostrarModal(){
     document.getElementById("modal").classList.add("visible");
@@ -7,4 +12,16 @@ function mostrarModal(){
 
 function ocultarModal(){
     document.getElementById("modal").classList.remove("visible");
+}
+
+function seleccionarImagen(){
+    impSeleccionImg.click();
+}
+
+function tomarImagen(){
+    if(impSeleccionImg.value){
+        txtSeleccionImg.innerHTML = impSeleccionImg.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    } else {
+        txtSeleccionImg.innerHTML = "No ha seleccionado ninguna imágen";
+    }
 }
