b3a0f8873c2545a8240bd8ef0d386cc89aa673c2
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-11-17 23:01:34 -0300

    Revert "ae"
    This reverts commit 0d7da8dd1050bcd93b6fc050fb391c136a971ecb.

--- src/historial.js
+++
@@ -1,9 +0,0 @@
-class Historial {
-    
-    constructor(idLeccion, idAlumno) {
-      
-      this.idLeccion = idLeccion;
-      this.idAlumno = idAlumno;
-            
-    }
-  }  
---
+++ src/pages/crearLecciones/crearLecciones.js
@@ -0,0 +1,27 @@
+document.getElementById("btn-crear-leccion").addEventListener("click", mostrarModal);
+document.getElementById("btn-ocultar-modal").addEventListener("click", ocultarModal);
+document.getElementById("imp-file").addEventListener("click", seleccionarImagen);
+document.getElementById("fil-img-leccion").addEventListener("change", tomarImagen);
+
+const impSeleccionImg = document.getElementById("fil-img-leccion");
+const txtSeleccionImg = document.getElementById("imagen-seleccionada");
+
+function mostrarModal(){
+    document.getElementById("modal").classList.add("visible");
+}
+
+function ocultarModal(){
+    document.getElementById("modal").classList.remove("visible");
+}
+
+function seleccionarImagen(){
+    impSeleccionImg.click();
+}
+
+function tomarImagen(){
+    if(impSeleccionImg.value){
+        txtSeleccionImg.innerHTML = impSeleccionImg.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    } else {
+        txtSeleccionImg.innerHTML = "No ha seleccionado ninguna imágen";
+    }
+}
